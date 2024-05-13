f4392f193a90a00d9df0d98b872d0b06cc565c6c
Andreas Gohr <andi@splitbrain.org>
2024-05-13 12:01:35 +0200

    fix typo in config meta data

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,4 +1,4 @@
 <?php
 
 $meta['all_tables'] = array('onoff');
-$meta['disabled'] = array('multicheckbox', '_choices' => array('insert_after', 'inselt_before', 'edit', 'remove'));
+$meta['disabled'] = array('multicheckbox', '_choices' => array('insert_after', 'insert_before', 'edit', 'remove'));
